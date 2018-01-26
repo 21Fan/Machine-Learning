4d07b215947bff6b5eca53ef4d5c77a07ea3a337
21Fan <fan1534272740@163.com>
2018-01-26 15:39:53 +0800

    5

--- 018卷积神经网络.py
+++ 018卷积神经网络.py
@@ -6,7 +6,7 @@ mnist = input_data.read_data_sets('MNIST_data',one_hot=True)
 def compute_accuracy(v_xs,v_ys):
     global prediction
     #预测值:概率
-    y_pre = sess.run(prediction,feed_dict={xs:v_xs,keep_prob:1})
+    y_pre = sess.run(prediction,feed_dict={xs:v_xs})
     #tf.argmax(vector, 1)返回的是vector中的最大值的下标
     #如果vector是一个向量，那就返回一个值，如果是一个矩阵，那就返回一个向量
     #tf.equal(A, B)对比向量各项相等与否
@@ -14,7 +14,7 @@ def compute_accuracy(v_xs,v_ys):
     #tf.reduce_mean求平均
     #tf.cast(x,dtype,name)类型转换
     accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
-    result = sess.run(accuracy,feed_dict={xs:v_xs,ys:v_ys,keep_prob:1})
+    result = sess.run(accuracy,feed_dict={xs:v_xs,ys:v_ys})
     return result
 
 def weight_variable(shape):
@@ -32,9 +32,9 @@ def conv2d(x,W):
 def max_pool_2x2(x):
     return tf.nn.max_pool(x,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
 
-xs = tf.placeholder(tf.float32,[None,784]) # 28*28
+xs = tf.placeholder(tf.float32,[None,784])/255 # 28*28
 ys = tf.placeholder(tf.float32,[None,10])
-keep_prob = tf.placeholder(tf.float32)
+
 
 x_image = tf.reshape(xs,[-1,28,28,1])
 #print(x_image.shape) 
@@ -59,12 +59,12 @@ b_fc1 = bias_variable([1024])
 #[n_samples,7,7,64]->>[n_samples,7*7*64]
 h_pool2_flat = tf.reshape(h_pool2,[-1,7*7*64])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat,W_fc1)+b_fc1)
-h_fc1_drop = tf.nn.dropout(h_fc1,keep_prob)
+
 
 ##func1 layer##
 W_fc2 = weight_variable([1024,10])
 b_fc2 = bias_variable([10])
-prediction = tf.nn.softmax(tf.matmul(h_fc1_drop,W_fc2)+b_fc2)
+prediction = tf.nn.softmax(tf.matmul(h_fc1,W_fc2)+b_fc2)
 
 #分类算法
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction),
