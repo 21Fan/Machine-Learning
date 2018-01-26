7622b7d3d8be2d02166e747bc04be328b5ac9931
21Fan <fan1534272740@163.com>
2018-01-26 17:15:58 +0800

    6

--- 018改.py
+++ 018改.py
@@ -32,7 +32,7 @@ def conv2d(x,W):
 def max_pool_2x2(x):
     return tf.nn.max_pool(x,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
 
-xs = tf.placeholder(tf.float32,[None,784])/255 # 28*28
+xs = tf.placeholder(tf.float32,[None,784]) # 28*28
 ys = tf.placeholder(tf.float32,[None,10])
 
 #转换形式[所有例子*28*28*黑白1]
@@ -40,31 +40,32 @@ x_image = tf.reshape(xs,[-1,28,28,1])
 
 
 ##conv1 layer##
-# patch 5x5,in size 1是image的厚度,out size 32高度
-W_conv1 = weight_variable([5,5,1,32])
-b_conv1 = bias_variable([32])
+# patch 5x5,in size 1是image的厚度,out size 25高度
+W_conv1 = weight_variable([5,5,1,25])
+b_conv1 = bias_variable([25])
 #非线性处理(x*W+b)
-h_conv1 = tf.nn.relu(conv2d(x_image,W_conv1)+b_conv1)#out size=28x28x32
-h_pool1 = max_pool_2x2(h_conv1)                     #out size=14*14*32
+h_conv1 = tf.nn.relu(conv2d(x_image,W_conv1)+b_conv1)#out size=28x28x25
+h_pool1 = max_pool_2x2(h_conv1)                     #out size=14*14*25
 
 ##conv2 layer##
-# [patch 5x5,in size 32是输入厚度,out size 64高度]
-W_conv2 = weight_variable([5,5,32,64])
-b_conv2 = bias_variable([64])
-h_conv2 = tf.nn.relu(conv2d(h_pool1,W_conv2)+b_conv2)#out size=14*14*64
-h_pool2 = max_pool_2x2(h_conv2)                     #out size=7*7*64
+# [patch 5x5,in size 25是输入厚度,out size 50高度]
+W_conv2 = weight_variable([5,5,25,50])
+b_conv2 = bias_variable([50])
+h_conv2 = tf.nn.relu(conv2d(h_pool1,W_conv2)+b_conv2)#out size=14*14*50
+h_pool2 = max_pool_2x2(h_conv2)                     #out size=7*7*50
 
 ##func1 layer##
-W_fc1 = weight_variable([7*7*64,1024])
-b_fc1 = bias_variable([1024])
-#[n_samples,7,7,64]->>[n_samples,7*7*64]
-h_pool2_flat = tf.reshape(h_pool2,[-1,7*7*64])
+W_fc1 = weight_variable([7*7*50,150])
+b_fc1 = bias_variable([150])
+#[n_samples,7,7,50]->>[n_samples,7*7*50]
+h_pool2_flat = tf.reshape(h_pool2,[-1,7*7*50])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat,W_fc1)+b_fc1)
 
 
-##func1 layer##
-W_fc2 = weight_variable([1024,10])
+##func layer##
+W_fc2 = weight_variable([150,10])
 b_fc2 = bias_variable([10])
+#softmax取权重最大的
 prediction = tf.nn.softmax(tf.matmul(h_fc1,W_fc2)+b_fc2)
 
 #分类算法
